a8c856e672ed8e2fca06ab88f5ba4c0ff3c2fa98
marivi73 <mariavhdez220103@gmail.com>
2026-05-12 13:40:54 +0100

    Corrige hash duplicado en UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -28,7 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => 'password',
             'remember_token' => Str::random(10),
             'phone' => fake()->phoneNumber(),
             'role' => fake()->randomElement([
